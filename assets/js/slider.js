4937992e0c22796ac96a51e459ea93b5f29cfe14
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-08-26 18:52:09 -0300

    Alterado nome id slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,6 +1,11 @@
+//Slider
 $(document).ready(function() {
-    $('#slider').lightSlider({
+    $('#slider, #slider2').lightSlider({
         autoWidth:true,
         loop:true,
+        onSliderLoad: function() {
+            $('#slider, slider2').removeClass('cS-hidden');
+        }
     });
-});
+  });
+
