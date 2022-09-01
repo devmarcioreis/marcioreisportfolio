20c9bd50f58d88298cc32d36ec93d8bac0029e31
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-09-01 17:45:22 -0300

    Desenvolvido uma timeline

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,12 +78,7 @@ function scrollToHref(element) {
     return document.querySelector(id).offsetTop;
 };
 
-//Habilidades
-const progressBars = document.querySelectorAll('#progress-bar');
 
-for (let progressBar of progressBars) {
-    progressBar.style.width = progressBar.getAttribute('data-percent') + '%';
-}
 
 
 
